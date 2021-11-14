3504c2e0a79d736286e830ae7141c9cbbc76a03f
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2021-11-14 14:36:45 +0100

    Add graph resizing

--- src/app/components/tab/post-content/tab-post-content.component.ts
+++ src/app/components/tab/post-content/tab-post-content.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { AfterViewChecked, AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { LegendPosition } from '@swimlane/ngx-charts';
 import { LinearGraphDataDto } from 'src/app/dto/LinearGraphDataDto';
 import { GraphSeriesService } from 'src/app/services/graph-series.service';
+
+const WINDOW_BREAKING_POINT: number = 1000;
+const WINDOW_BREAKING_POINT_LEGEND: number = 1300;
 
 @Component({
   selector: 'app-tab-post-content',
@@ -9,21 +12,54 @@ import { GraphSeriesService } from 'src/app/services/graph-series.service';
   styleUrls: ['./tab-post-content.component.css']
 })
 export class TabPostContentComponent implements OnInit {
-  step : number = 0;
+  @Input() symbol!: string;
+  step: number = 0;
+  legendPosition: LegendPosition = LegendPosition.Right;
   graphData!: LinearGraphDataDto;
-  view: any[2] = [700, 300];
+  view: any[2];
   colorScheme = {
     domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
   };
 
   constructor(private service: GraphSeriesService) {
-   }
-
-  ngOnInit(): void {
-    this.service.getSeries("PKO").subscribe((data) => this.graphData = data);
   }
 
-  setStep(step:number) : void{
-    this.step= step;
+  ngOnInit(): void {
+    this._calculateGraphDimension();
+  }
+
+  setStep(step: number): void {
+    this.step = step;
+  }
+
+  openGraph() {
+    if (this.symbol == undefined) {
+      console.log("Problem with post data. Symbol not found");
+      return;
+    }
+    if (this.graphData == undefined) {
+      this.service.getSeries(this.symbol).subscribe((data) => {
+        this.graphData = data;
+        this.setStep(1);
+      });
+    }
+  }
+
+  onResize(event: any) {
+    this._calculateGraphDimension();
+  }
+
+  _calculateGraphDimension() {
+    console.log('Changing')
+    if (window.outerWidth < WINDOW_BREAKING_POINT) {
+      this.view = [window.outerWidth * 0.8, window.outerWidth * 0.8];
+    } else {
+      this.view = [window.outerWidth * 0.5, WINDOW_BREAKING_POINT/2];
+    }
+    if(window.outerWidth < WINDOW_BREAKING_POINT_LEGEND){
+      this.legendPosition = LegendPosition.Below;
+    } else {
+      this.legendPosition = LegendPosition.Right;
+    }
   }
 }
--- src/app/dto/PostDto.ts
+++ src/app/dto/PostDto.ts
@@ -1,6 +1,7 @@
 export interface PostDto{
   id: number;
   name: string;
+  symbol: string;
   value: number;
   date: Date;
   note: string;
